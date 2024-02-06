856bc8cf82f40e92d09ee0e3bbcce33ce727bf10
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-02-06 19:25:33 +0500

    Update route.ts

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -9,10 +9,7 @@ export async function GET(){
             }
 
         )
-                response.cookies.set("toke", "token", {
-            httpOnly: true, maxAge:315360000000000 
-            
-        })
+response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})
         return response
     } catch (error:any) {
         return NextResponse.json({error:error.message},{status:500})
